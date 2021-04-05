d49dadc0cbf9f62fbd7d09a84dbd613a7a898d4f
Calvin Collins <cjc8@williams.edu>
2021-04-05 09:40:51 -0700

    addressed comments

--- LUX/Classes/TableViews/View Models/LUXSegmentedTableViewModel.swift
+++ LUX/Classes/TableViews/View Models/LUXSegmentedTableViewModel.swift
@@ -6,20 +6,23 @@
 //
 
 import Foundation
-import Combine
 import FlexDataSource
 
 open class LUXSegmentedTableViewModel: LUXRefreshableTableViewModel {
     public var selectedIndex: Int = 0 {
         didSet {
-            dataSource = segments?[selectedIndex]
-            tableView?.reloadData()
+            if let count = segments?.count, selectedIndex < count {
+                dataSource = segments?[selectedIndex]
+                tableView?.reloadData()
+            }
         }
     }
     public var segments: [FlexDataSource]? {
         didSet {
-            dataSource = segments?[selectedIndex]
-            tableView?.reloadData()
+            if let count = segments?.count, selectedIndex < count {
+                dataSource = segments?[selectedIndex]
+                tableView?.reloadData()
+            }
         }
     }
 }
