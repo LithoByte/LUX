c51602b5553a1ee2d3acbe7c38c6550aae3e7347
Calvin Collins <calvin.john.collins@gmail.com>
2021-10-18 16:04:43 -0700

    added fns

--- LUX/Classes/Networking/CombineNetCall+Publishers.swift
+++ LUX/Classes/Networking/CombineNetCall+Publishers.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import FunNet
+import Combine
+
+@available(iOS 13.0, *) extension CombineNetCall {
+    public func modelPublisher<T: Codable>() -> AnyPublisher<T, Never> {
+        return LUX.modelPublisher(from: publisher.$data.eraseToAnyPublisher())
+    }
+    public func unwrappedModelPublisher<T: Codable, U: Codable>(unwrapper: @escaping (T) -> U) -> AnyPublisher<U, Never> {
+        return self.modelPublisher().map(unwrapper).eraseToAnyPublisher()
+    }
+}
